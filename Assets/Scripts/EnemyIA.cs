5f154cc5e27f6bb96245c63d4103bc527170e534
Javier <javiersantacruz87@gmail.com>
2016-11-23 14:28:08 -0300

    Se agrerga FPC, y script de IA

---
+++ Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyIA : MonoBehaviour {
+
+	public Transform target;
+	public float moveSpeed = 3;
+	public float rotationSpeed = 3;
+	public float distance = 5f;
+	public bool Enabled = false;
+	public bool Detect = false;
+	private Transform myTransform;
+
+
+	void Awake()
+	{
+		myTransform = this.GetComponent<Transform>();
+	}
+
+
+	void Start()
+	{
+		target = GameObject.FindWithTag("Player").transform;
+	}
+
+
+	void Update()
+	{
+		if (Enabled)
+		{
+			myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+			RaycastHit info;
+			Debug.DrawRay(transform.position, transform.forward * distance, Color.red, 0.1f);
+			if (Physics.Raycast(transform.position, transform.forward, out info, distance))
+			{
+				if (info.collider.tag == "Player")
+				{
+					Detect = true;
+				}
+				else
+				{
+					Detect = false;
+				}
+			}
+			else
+			{
+				Detect = false;
+			}
+		}
+		if (Enabled && Detect)
+		{
+			myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			Enabled = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			Enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/cambioColor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cambioColor : MonoBehaviour {
+
+	public int velocidad = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			gameObject.GetComponent<Renderer> ().material.color = Color.red;
+		}
+		if(Input.GetKeyDown(KeyCode.G))
+		{
+			gameObject.GetComponent<Renderer> ().material.color = Color.green;
+		}
+		if(Input.GetKeyDown(KeyCode.B))
+		{
+			gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+		}
+
+		transform.Translate (new Vector3 (0, 0, Time.deltaTime * velocidad));
+	
+	}
+
+	void OnCollisionEnter(){
+		print ("Impacto!! AHHHHHHH");
+	}
+}
